77bd79d47abdcfaead8da5bf6345041705bea11b
Lep-1993 <pierde.an93@gmail.com>
2026-05-16 09:48:16 +0200

    Create test_telegram.py

---
+++ test_telegram.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+message = "🚀 Test riuscito: Anime Radar è attivo!"
+
+url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+payload = {
+    "chat_id": chat_id,
+    "text": message
+}
+
+response = requests.post(url, json=payload)
+
+print(response.json())
